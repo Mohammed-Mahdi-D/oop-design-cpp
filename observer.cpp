0d5ca1f420a59fcfaf32283adbf70568cad73d70
Mohammed Mahdi <djaballah.mohammed.mahdi@gmail.com>
2024-09-17 06:03:55 +0100

    created a concrete class  of Subject, containing a vector of its observers and methods to subscribe/unsibscribe to an observer, and a notify method

--- observer.cpp
+++ observer.cpp
@@ -1,19 +1,51 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 
 
 class Observer
 {
     public:
-        virtual void update(float temp, float humidity) = 0;
+        virtual void update(float temp, float humidity) = 0; // change those as needded
 };
 
 class Subject 
 {
-    virtual ~Subject() {}
-    virtual void subscribe(Observer* observer) = 0;
-    virtual void unSubscribe(Observer* observer) = 0;
-    virtual void notify() = 0;
+    public:
+        virtual ~Subject() {}
+        virtual void subscribe(Observer* observer) = 0;
+        virtual void unSubscribe(Observer* observer) = 0;
+        virtual void notify() = 0;
+};
+
+
+class SubjectChild : public Subject
+{
+    public: 
+        std::vector<Observer*> observers;
+
+        void subscribe(Observer* observer) override
+        {
+            observers.push_back(observer);
+        }
+
+        void unSubscribe(Observer* observer) override
+        {
+            auto it = std::remove(observers.begin(), observers.end(), observer);
+            if (it != observers.end()) {
+                observers.erase(it, observers.end());
+            }
+        }
+
+        void notify() override
+        {
+            for(auto& observer : observers)
+            {
+                observer->update(22.4, 75.0); // change the values as needded
+            }
+        }
+
+
 };
 
 
